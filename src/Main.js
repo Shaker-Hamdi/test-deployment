58236714ec3ca1f2a4dd2f03d3a6e80a48e73cdb
Shaker Hamdi <106151230+Shaker-Pelcro@users.noreply.github.com>
2023-02-09 15:59:35 +0200

    feat: new feature on old version on branch 1.x

--- src/Main.js
+++ src/Main.js
@@ -6,6 +6,7 @@ export const Main = () => {
       <h1>Hello World</h1>
       <h2>This is a fix example</h2>
       <p>A small feture</p>
+      <p>This is a feature on the old version 1.x</p>
     </>
   );
 };
