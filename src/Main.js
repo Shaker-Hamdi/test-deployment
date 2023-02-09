07d082530c95028977c21f9380859bdb8407e383
Shaker Hamdi <106151230+Shaker-Pelcro@users.noreply.github.com>
2023-02-09 14:34:06 +0200

    fix: a fix example

--- src/Main.js
+++ src/Main.js
@@ -2,6 +2,9 @@ import React from "react";
 
 export const Main = () => {
   return (
-    <h1>Hello World</h1>
+    <>
+      <h1>Hello World</h1>
+      <h2>This is a fix example</h2>
+    </>
   );
 };
