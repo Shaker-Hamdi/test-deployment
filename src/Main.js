93d114374c82f85ef7e949ea12c1f544121ce37a
Shaker Hamdi <106151230+Shaker-Pelcro@users.noreply.github.com>
2023-02-09 15:03:36 +0200

    fix: fix something

--- src/Main.js
+++ src/Main.js
@@ -6,6 +6,7 @@ export const Main = () => {
       <h1>Hello World</h1>
       <h2>This is a fix example</h2>
       <h3>This is a breaking change on "next" branch</h3>
+      <h4>This is a bug fix on @next channel</h4>
     </>
   );
 };
